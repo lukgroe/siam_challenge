df52968b89a15b5542274ee3d47485ea75ae7fc7
Levin <levin.kiefer@student.kit.edu>
2021-03-14 12:11:55 +0100

    small changes in mixed_strategy
    Fixed bugs, added return values

--- Mixed Strategy/mixed_strategy.m
+++ Mixed Strategy/mixed_strategy.m
@@ -1,4 +1,4 @@
-function simObj = mixed_strategy(simObj, mu, c) % mu only given to compare approximation to exact value.
+function [simObj, mu_t, c_t] = mixed_strategy(simObj, mu, c) % mu only given to compare approximation to exact value.
     w_const = ones(simObj.d,1)/simObj.d; % Only really important thing is that it remains constant.
     simObj = simObj.reset(); % reset simulation environment
     s_w_delta_zero = zeros(simObj.d, simObj.T); %delta s for delta w equals zero
@@ -12,7 +12,7 @@ function simObj = mixed_strategy(simObj, mu, c) % mu only given to compare appro
     % Simulate.
     for i=1:simObj.T
         
-        if i<simObj.T/5
+        if i<simObj.T/2
             simObj = simObj.step(w_const); % Strategy only temporary.
         else
             w_random = poissrnd(10,simObj.d, 1) + ones(simObj.d,1);
@@ -21,10 +21,10 @@ function simObj = mixed_strategy(simObj, mu, c) % mu only given to compare appro
         end
         
         % Compute difference of weights.
-        if simObj.t == 0
+        if simObj.t == 1
             w_delta = zeros(simObj.d, 1);
         else
-            w_delta = simObj.w_cur - simObj.w_hist(:,simObj.t);
+            w_delta = simObj.w_cur - simObj.w_hist(:,simObj.t-1);
         end
         
             
@@ -46,13 +46,15 @@ function simObj = mixed_strategy(simObj, mu, c) % mu only given to compare appro
         
         % Calculate current mu and c approximation from this round.
         for j = 1:(simObj.d)
-            if w_delta(j) == 0 
+            if w_delta(j) == 0
                 mu_t(j,1) = mu_t(j,2) * mu_t(j,1) + diff(j); 
                 mu_t(j,2) = mu_t(j,2) + 1;
                 mu_t(j,1) = mu_t(j,1) / mu_t(j,2);
             end
-            for k=1:min(w_zero_counter-1, w_nonzero_counter-1)
-                c_t(j) = c_t(j) + (s_w_delta_zero(j,k)-s_w_delta_nonzero(j,k))/w_w_delta_nonzero(j,k);
+            if w_zero_counter(j)-1 ~= 0 && w_nonzero_counter(j)-1 ~= 0
+                for k=1:max(w_zero_counter(j)-1, w_nonzero_counter(j)-1)
+                    c_t(j) = c_t(j) + (s_w_delta_zero(j,mod(k, w_zero_counter(j)-1)+1)-s_w_delta_nonzero(j,mod(k, w_nonzero_counter(j)-1)+1))/w_w_delta_nonzero(j,mod(k, w_nonzero_counter(j)-1)+1);
+                end
             end
         end
         
