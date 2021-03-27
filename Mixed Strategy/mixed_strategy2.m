6dc6e6b8cf6c0eb358959ae09bcc2c1c21241ce1
Levin <levin.kiefer@student.kit.edu>
2021-03-27 18:09:13 +0100

    added comments, fixed bug, added variable cor goals

--- Mixed Strategy/mixed_strategy2.m
+++ Mixed Strategy/mixed_strategy2.m
@@ -1,31 +1,31 @@
-function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda) % mu only given to compare approximation to exact value.
-    %w_const = ones(simObj.d,1)/simObj.d; % Only really important thing is that it remains constant.
-    w_next = zeros(simObj.d,1);
-    w = max(1/floor(1/lambda),1/simObj.d);
-    while sum(w_next)<1-w
-        w_next(randi([1,simObj.d])) = w;
+function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda)
+    d = simObj.d;
+    T = simObj.T;
+    w_next = zeros(d,1);
+    w = min(floor(1+3*lambda),1/d);
+    while sum(w_next)<1-w % Random initial strategy; equal weights over floor(1+3*lambda) random shares
+        w_next(randi([1,d])) = w;
     end
     simObj = simObj.reset(); % reset simulation environment
-    s_w_delta_nonzero = zeros(simObj.d, simObj.T); % Delta of s for delta w not equal zero
-    w_w_delta_nonzero = zeros(simObj.d, simObj.T); % Delta of w for delta w not equal zero
-    w_nonzero_counter = ones(simObj.d,1); % Counter for number of data points
-    mu_t = zeros(simObj.d, 2); % [mu approximation, number of data points used]
-    c_t = zeros(simObj.d, 1);
-    cov = zeros(simObj.d, simObj.d);
-    cov_counter = zeros(simObj.d, simObj.d);
-    diff_hist = zeros(simObj.d, simObj.T);
-    cor = zeros(simObj.d, simObj.d);
+    s_w_delta_nonzero = zeros(d, T); % Delta of s for delta w not equal zero
+    w_w_delta_nonzero = zeros(d, T); % Delta of w for delta w not equal zero
+    w_nonzero_counter = ones(d,1); % Counter for number of data points
+    mu_t = zeros(d, 2); % [mu approximation, number of data points used]
+    c_t = zeros(d, 1); % Estimator for c
+    cov = zeros(d, d); % Estimator for covariance
+    cov_counter = zeros(d, d); % Counter of number of data points considered in cov
+    cor = zeros(d, d); % Estimator for correlation matrix
     
     % Simulate.
-    for i=1:simObj.T
+    for i=1:T
         
-        if i<simObj.T/3 || mod(i,10) ~= 0
-            simObj = simObj.step(w_next); 
+        if i<min(T/4, lambda*200) || mod(i,10) ~= 0
+            simObj = simObj.step(w_next);
         else
             
-            c_t = zeros(simObj.d, 1); %currently unnecessary calculation of c
-            for j=1:simObj.d
-                for k=1:simObj.d % Correlation is being calculated
+            c_t = zeros(d, 1); %currently unnecessary calculation of c
+            for j=1:d
+                for k=1:d % Correlation is being calculated
                     cor(j,k) = cov(j,k)/sqrt(cov(j,j)*cov(k,k));
                 end
                 if w_nonzero_counter(j)-1 ~= 0
@@ -37,12 +37,16 @@ function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda) % mu on
             end
             
             
-            w_next = zeros(simObj.d,1);
-            mu_sort = sortrows([mu_t(:,1), (1:simObj.d)'],1);
-            for j=1:floor(1/lambda)
-                mu_high_ind = mu_sort(simObj.d-j+1,2);
+            w_next = zeros(d,1);
+            mu_sort = sortrows([mu_t(:,1), (1:d)'],1);
+            for j=1:floor(1+3*lambda)
+                mu_high_ind = mu_sort(d-j+1,2);
                 w_next(mu_high_ind) = 1;
-                [~, min_cor_ind] = min(cor(:, mu_high_ind));
+                cor_smaller = -1*ones(d, 1)*lambda > cor(:, mu_high_ind);
+                [maximum, min_cor_ind] = max(mu_t.*cor_smaller);
+                if maximum == 0
+                    [~, min_cor_ind] = min(abs(cor(:, mu_high_ind)-max(lambda*ones(d, 1),ones(d,1))));
+                end
                 w_next(min_cor_ind) = 1;
             end
             w_next = w_next/sum(w_next);
@@ -51,7 +55,7 @@ function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda) % mu on
         
         % Compute difference of weights.
         if simObj.t == 1
-            w_delta = zeros(simObj.d, 1);
+            w_delta = zeros(d, 1);
         else
             w_delta = simObj.w_cur - simObj.w_hist(:,simObj.t-1);
         end
@@ -59,10 +63,9 @@ function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda) % mu on
             
         % Prepare data by looking at the differences.
         diff = log(simObj.s_cur) - log(simObj.s_hist(:,simObj.t));
-        diff_hist(:,i) = diff;
         
         % Put deltas in different matrices for c and cov
-        for j=1:simObj.d
+        for j=1:d
             if w_delta(j) ~= 0
                 s_w_delta_nonzero(j,w_nonzero_counter(j)) = diff(j);
                 w_w_delta_nonzero(j,w_nonzero_counter(j)) = w_delta(j);
@@ -72,16 +75,17 @@ function [simObj, mu_t, c_t, cov, cor] = mixed_strategy2(simObj, lambda) % mu on
 
         
         % Calculate current mu approximation from this round.
-        for j = 1:(simObj.d)
+        for j = 1:(d)
             if w_delta(j) == 0
-                mu_t(j,1) = mu_t(j,2) * mu_t(j,1) + diff(j); 
+                mu_t(j,1) = mu_t(j,2) * mu_t(j,1) + diff(j);
                 mu_t(j,2) = mu_t(j,2) + 1;
                 mu_t(j,1) = mu_t(j,1) / mu_t(j,2);
             end
         end
         
-        for j=1:simObj.d
-            for k=1:simObj.d
+        % Calculate cov from current round        
+        for j=1:d
+            for k=1:d
                 if w_delta(j) == 0 && w_delta(k) == 0
                     cov(j,k) = (cov(j,k)*max(1, cov_counter(j,k)-1) + (diff(j)-mu_t(j,1))*(diff(k)-mu_t(k,1)))/max(1,cov_counter(j,k));
                     cov_counter(j,k) = cov_counter(j,k)+1;
--- Mixed Strategy/run_strategy_mod2.m
+++ Mixed Strategy/run_strategy_mod2.m
@@ -27,8 +27,7 @@ sim_obj = MarketSimulator(T,s0,model_params);
 %sim_obj = simulation_strategy(sim_obj, mu, c, M);
 %sim_obj = parameter_estimation_strategy(sim_obj, mu);
 %sim_obj = mcts(sim_obj, mu, c);
-%[sim_obj, mu_t, c_t, cov, cor] = mixed_strategy2(sim_obj, 0.3);
-[sim_obj, mu_t, c_t, cov, cor] = real_strategy(sim_obj);
+[sim_obj, mu_t, c_t, cov, cor] = mixed_strategy2(sim_obj, .1);
 
 % Plot simulated price history
 figure(1);
@@ -67,7 +66,7 @@ cumret_array = zeros(nsims,1);
 
 for k=1:nsims
     % Store each simulation's result in array
-    sim_obj = real_strategy(sim_obj,mu,c);
+    sim_obj = mixed_strategy2(sim_obj,lambda);
     cumret_array(k) = sim_obj.R_hist(end);
 end
 
